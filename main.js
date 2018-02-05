3f699c00aad42e84496a1d4a3db620f9cc8c2b4b
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-05 15:30:50 +0000

    fixing bugs

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ global.main = function() {
 	if (! contact.vars.learner){
 		//if contact is not a learner, she's a tutor ... in the group
 
-		var question = require('ext/learninglinks/question');
+		var question = require('./question');
 		keyword = keyword.trim();
 		var questionCode = parseInt(keyword);
 
 		var groupLearnerQuestion = question.getQuestion(questionCode);
 		//if (groupLearnerQuestion is valid proceed else alert teacher that question code is not available);
-		
+
 		console.log(groupLearnerQuestion);
 		sendReply("Testing Fetching question from JSON");
 		//ideally, tutor should get tutor prompt ... 

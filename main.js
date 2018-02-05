a09748d2ab8f7162449c000db6710ec8ca8d5867
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-05 15:25:09 +0000

    testing reading json

--- main.js
+++ main.js
@@ -7,7 +7,16 @@ global.main = function() {
 	//check if is learner or tutor
 	if (! contact.vars.learner){
 		//if contact is not a learner, she's a tutor ... in the group
-		sendReply("tutor Requesting for question for the group");
+
+		var question = require('ext/learninglinks/question');
+		keyword = keyword.trim();
+		var questionCode = parseInt(keyword);
+
+		var groupLearnerQuestion = question.getQuestion(questionCode);
+		//if (groupLearnerQuestion is valid proceed else alert teacher that question code is not available);
+		
+		console.log(groupLearnerQuestion);
+		sendReply("Testing Fetching question from JSON");
 		//ideally, tutor should get tutor prompt ... 
 	}
 

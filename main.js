3fbd889da3ee6c114140013d04017ea12d13a5f6
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-06 12:41:09 +0000

    commit via shell script [WIP]

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ global.main = function() {
 	var keyword = content; //get word1 instead .. 
 
 	//remember to trim the string ... 
-	console.log(contact.name);
+	console.log(contact.phone_number);
 	//check if is learner or tutor
 	if (!contact.vars.learner){
 		//if contact is not a learner, she's a tutor ... in the group

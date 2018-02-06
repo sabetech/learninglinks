8a4c132796a6552b0869a5ba5cf36385cdd7914e
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-06 00:21:45 +0000

    access json properly

--- question.js
+++ question.js
@@ -6,20 +6,15 @@
 exports.getQuestion = function(questionCode) { 
     
 	var level1Numeracy = {
-						  questions: 
-						    {
-						      q2002:{
-						          question: "How many? ($ $ $ $) \n1. 5\n2. 4\n3. 3",
-						          answer: "2",
-						          correctAnswerResponse: "Well done child"
-						        }
+					      	q2002:{
+						        question: "How many? ($ $ $ $) \n1. 5\n2. 4\n3. 3",
+						        answer: "2",
+						        correctAnswerResponse: "Well done child"
 						    },
-						    {
-						      q2003:{
-						          question: "How many? ($ $ $) \n1. 5\n2. 4\n3. 3",
-						          answer: "3",
-						          correctAnswerResponse: "Nerh I'm sorry"
-						        }
+						    q2003:{
+						        question: "How many? ($ $ $) \n1. 5\n2. 4\n3. 3",
+						        answer: "3",
+						        correctAnswerResponse: "Nerh I'm sorry"
 						    }
 						};
 	

8aa72ea47560a24cb42bc93a86c3066464ee71fe
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-06 00:25:17 +0000

    commit via shell script [WIP]

--- question.js
+++ question.js
@@ -7,12 +7,12 @@ exports.getQuestion = function(questionCode) {
     
 	var level1Numeracy = {
 					      	q2002:{
-						        question: "How many? ($ $ $ $) \n1. 5\n2. 4\n3. 3",
+						        question: "How many? ($ $ $ $) \r\n1. 5\r\n2. 4\r\n3. 3",
 						        answer: "2",
 						        correctAnswerResponse: "Well done child"
 						    },
 						    q2003:{
-						        question: "How many? ($ $ $) \n1. 5\n2. 4\n3. 3",
+						        question: "How many? ($ $ $) \r\n1. 5\r\n2. 4\r\n3. 3",
 						        answer: "3",
 						        correctAnswerResponse: "Nerh I'm sorry"
 						    }

0358dec95aa9a27129b0cc3db9d964383f0a60b5
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-06 00:11:14 +0000

    access json properly

--- question.js
+++ question.js
@@ -6,23 +6,21 @@
 exports.getQuestion = function(questionCode) { 
     
 	var level1Numeracy = {
-						  questions: [
+						  questions: 
 						    {
 						      q2002:{
 						          question: "How many? ($ $ $ $) \n1. 5\n2. 4\n3. 3",
 						          answer: "2",
 						          correctAnswerResponse: "Well done child"
 						        }
-						    }
-						        ,
+						    },
 						    {
 						      q2003:{
 						          question: "How many? ($ $ $) \n1. 5\n2. 4\n3. 3",
 						          answer: "3",
 						          correctAnswerResponse: "Nerh I'm sorry"
 						        }
-						      }
-						  ]
+						    }
 						};
 	
 	var jsonObject = level1Numeracy.questions['q'+questionCode];

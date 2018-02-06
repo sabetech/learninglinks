e53f0f16b2239fe74c0fde2d7071c6d64ec46d47
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-06 12:39:33 +0000

    commit via shell script [WIP]

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ global.main = function() {
 	var keyword = content; //get word1 instead .. 
 
 	//remember to trim the string ... 
-
+	console.log(contact.name);
 	//check if is learner or tutor
-	if (! contact.vars.learner){
+	if (!contact.vars.learner){
 		//if contact is not a learner, she's a tutor ... in the group
 
 		var question = require('./question');

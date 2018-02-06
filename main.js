3a14c220249daebbcd68a7d95f51b246fbc109ed
Albert_Mens <albert.mensahansah@gmail.com>
2018-02-06 13:02:22 +0000

    commit via shell script [WIP]

--- main.js
+++ main.js
@@ -8,9 +8,24 @@ global.main = function() {
 	if (!contact.vars.learner){
 		//if contact is not a learner, she's a tutor ... in the group
 
-		sendReply("Are you working ... ")
+		var question = require('./question');
+		keyword = keyword.trim();
+		var questionCode = keyword;
 
+		var groupLearnerQuestion = question.getQuestion(questionCode);
+		//if (groupLearnerQuestion is valid proceed else alert teacher that question code is not available);
 
+		sendReply(groupLearnerQuestion.question);
+
+		//get group members to send the message to them ...
+		var group = project.getOrCreateGroup("Test Group");
+		cursor = group.queryContacts();
+
+		//send question to everyone in the group... 
+		while(cursor.hasNext()){
+			var learner_contact = cursor.next();
+			sendSMS(learner_contact.phone_number,groupLearnerQuestion.learner_question);
+		}
 
 	}else{
 		sendReply("Requesting for questions is now done by Tutor-Mentors");

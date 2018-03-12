f55c4be15e871e33ad2f91da0cbd34306e5af045
Albert_Mens <albert@votomobile.org>
2018-03-12 11:52:28 +0000

    made some slight changes to the js code ..

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ var questionBase = null;
 global.main = function() {
 
 	var keyword = word1;
+	if (keyword == ""){
+		return;
+	}
+	if (keyword == null){
+		return;
+	}
+	if (typeof keyword === 'undefined' ){
+		return;
+	}
+
 	keyword = keyword.trim();
+	
 
 	questionBase = require('./question');
 	//check if is learner or tutor
@@ -59,7 +70,8 @@ global.main = function() {
 			console.log("contact is not part of the test group");
 			return;
 		}
-	
+		
+		//check if learner has received a question ... 
 		var answerKey = keyword;
 		//get question code from contact ...
 		var learnerQuestion = getQuestionObject(contact.vars.current_question_code);
@@ -68,7 +80,7 @@ global.main = function() {
 		if (learnerQuestion.answer == answerKey){
 			
 			if (typeof contact.vars.cum_points === 'undefined'){
-				contact.vars.cum_points = 5	
+				contact.vars.cum_points = 5;
 			}else{
 				contact.vars.cum_points = parseInt(contact.vars.cum_points) + 5;
 			}
@@ -83,6 +95,11 @@ global.main = function() {
 			sendReply(learnerQuestion.incorrectAnswerResponse);
 			sendReply("Sorry, you earned no points.");
 
+			if (typeof contact.vars.cum_points === 'undefined'){
+				contact.vars.cum_points = 5;
+			}else{
+				contact.vars.cum_points = parseInt(contact.vars.cum_points) + 5;
+			}
 			//send message to tutor here ...
 			sendMessageToTutor(contact.name+" answered question "+learnerQuestion.code+" incorrectly and earned no points. Her Current points: "+contact.vars.cum_points);
 		}
@@ -153,120 +170,3 @@ function getLearnersTutor(){
 	return false;
 }
 
-
-/*
-
-
-
-
-
-//ASK QUESTION 2 Wait Timer
-addTimeoutHandler('askQuestion2', function() {
-	var questionCode = parseInt(contact.vars.current_question_code);
-	var individualQuestion2 = getQuestionObject(questionCode);
-	
-	console.log("Question 2 is being asked ...");
-    console.log(individualQuestion2.learner_question);
-
-    sendReply(individualQuestion2.learner_question);
-
-    waitForResponse('question2', {
-        timeoutMinutes: 3,
-        timeoutId: 'question2Reminder'
-    });
-});
-
-//RESPONSE HANDLER FOR QUESTION 2
-addResponseHandler('question2', function() {
-    //check if the person is a learner ...
-    var answerKey = word1;
-
-    //get question to be answered
-    var questionCode = contact.vars.current_question_code;
-	var individualQuestion2 = getQuestionObject(questionCode);
-    
-	//check if learner should have access to such a question code
-	if (individualQuestion2.answer == answerKey){
-		sendReply(individualQuestion2.correctAnswerResponse);
-		contact.vars.Cum_Points = parseInt(contact.vars.Cum_Points) + 5;
-	}else{
-		sendReply(individualQuestion2.incorrectAnswerResponse);
-	}
-
-	contact.vars.current_question_code = parseInt(contact.vars.current_question_code) + 1;
-	contact.save();
-
-	//send 3rd question ... 
-	waitForResponse('question3',{
-		timeoutMinutes: 15,
-		timeoutId: 'askQuestion3'
-	});
-    
-});
-
-//ASK QUESTION 3 Wait Timer
-addTimeoutHandler('askQuestion3', function() {
-	var questionCode = parseInt(contact.vars.current_question_code);
-	var individualQuestion3 = getQuestionObject(questionCode);
-
-    sendReply(individualQuestion3.learner_question);
-
-    waitForResponse('question3', {
-        timeoutMinutes: 15,
-        timeoutId: 'question3Reminder'
-    });
-});
-
-//RESPONSE HANDLER FOR QUESTION 3
-addResponseHandler('question3', function() {
-    //check if the person is a learner ...
-    var answerKey = word1;
-
-    //get question to be answered
-    var questionCode = contact.vars.current_question_code;
-	var individualQuestion3 = getQuestionObject(questionCode);
-    
-	//check if learner should have access to such a question code
-	if (individualQuestion3.answer == answerKey){
-		sendReply(individualQuestion3.correctAnswerResponse);
-		contact.vars.Cum_Points = parseInt(contact.vars.Cum_Points) + 5;
-	}else{
-		sendReply(individualQuestion3.incorrectAnswerResponse);
-	}
-
-	contact.vars.current_question_code = parseInt(contact.vars.current_question_code) + 1;
-	contact.save();
-
-	return;
-});
-
-//REMINDERS for QUESTION 2...
-addTimeoutHandler('question2Reminder', function() {
-	//get question to be answered
-    var questionCode = contact.vars.current_question_code;
-	var individualQuestion2 = getQuestionObject(questionCode);
-    sendReply("Reminder to answer question "+individualQuestion2.learner_question);
-    waitForResponse('question2', {
-        timeoutMinutes: 60,
-        timeoutId: 'timeout'
-    });
-});
-
-//REMINDERS for QUESTION 3...
-addTimeoutHandler('question3Reminder', function() {
-	//get question to be answered
-    var questionCode = contact.vars.current_question_code;
-	var individualQuestion3 = getQuestionObject(questionCode);
-    sendReply("Reminder to answer question "+individualQuestion3.learner_question);
-    waitForResponse('question3', {
-        timeoutMinutes: 60,
-        timeoutId: 'timeout'
-    });
-});
-
-//FINAL TIMEOUT
-addTimeoutHandler('timeout', function() {
-	//get question to be answered
-    sendReply("The question has expired because you didn't answer in time");
-});
-*/

b9727928a8b7f273d6daa97a0c7a4492ca4c28dd
Albert_Mens <albert@votomobile.org>
2018-05-16 14:37:44 +0000

    changing time to 30mins

--- main.js
+++ main.js
@@ -163,7 +163,7 @@ global.main = function() {
 		var scheduled_msg = project.scheduleMessage({
 		    content: individualQuestion.learner_question, 
 		    to_number: contact.phone_number, 
-		    start_time_offset: 120 //2mins change to 15 minutes
+		    start_time_offset: 1800 //30mins 
 		});
 
 		//put the timeout calls here ...

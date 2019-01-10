a488d3697299db9133e34addd7a1fc2905bd90c7
Albert <albert.mensahansah@gmail.com>
2019-01-10 18:50:50 +0000

    to 60 for Otis testing

--- main.js
+++ main.js
@@ -231,7 +231,7 @@ global.main = function() {
 				
 				message_type: 'call',
 				to_number: phoneNumber, 
-				start_time_offset: 1800, //30mins ...currently temporarily 1 min
+				start_time_offset: 60, //30mins ...currently temporarily 1 min
 				audio_url: audio_url,
 				route_id: "PN2607c459f76044a7" //the route id for handling ivr ...
 
@@ -248,7 +248,7 @@ global.main = function() {
 		var scheduled_msg = project.scheduleMessage({
 		    content: individualQuestion.vars.learner_question, 
 		    to_number: contact.phone_number, 
-		    start_time_offset: 1800 //30mins  //1 mins temp
+		    start_time_offset: 60 //30mins  //1 mins temp
 		});
 
 

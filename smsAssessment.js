3e0cf9c7942fd4bfa41194386e73a821fb7332c3
Albert <albert.mensahansah@gmail.com>
2019-01-11 22:34:19 +0000

    smsAssessment JS

--- smsAssessment.js
+++ smsAssessment.js
@@ -1,5 +1,41 @@
-global.main = function() {
 
-	console.log("begin here");
+function main() {
+    
+	console.log("fallback access sms assessment");
 
+     var keyword = word1;
+     
+     //When the keyword is START ... then proceed here ....
+     if (keyword === ""){
+		console.log("is keyword empty?");
+		sendReply("No keyword Sent");
+		message.delete();
+		return true;
+	}
+	if (keyword === null){
+		console.log("is keyword null?");
+		sendReply("No keyword Sent");
+		message.delete();
+		return true;
+	}
+	if (typeof keyword === 'undefined' ){
+		console.log("are you undefined?");
+		message.delete();
+		return true;
+	}
+	
+	keyword = keyword.trim();
+	
+	//start interaction here ...
+	if (keyword === "100"){
+        //begin ...	
+        console.log("fantastic");
+        
+        //getFirstQuestion
+        
+        
+	}else{
+	    return false;
+	}
+	
 }

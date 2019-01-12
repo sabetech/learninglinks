0ede85c0c5a01b34022dfb8298dbea2374857d7d
Albert <albert.mensahansah@gmail.com>
2019-01-12 00:57:36 +0000

    Test the SMS assessment questions

--- smsAssessment.js
+++ smsAssessment.js
@@ -1,6 +1,8 @@
+var questionBase = null;
+
 global.main = function() {
     
-	console.log("fallback access sms assessment");
+	console.log("SMS Assessment Program");
 
      var keyword = word1;
      
@@ -28,13 +30,63 @@ global.main = function() {
 	//start interaction here ...
 	if (keyword === "100"){
         //begin ...	
-        console.log("fantastic");
         
-        //getFirstQuestion
+        //check if this contact is eligible to partake SMS Assessment program
+
+        //check if learner has already taken this test ..
+
+        //get first question
+        questionBase = require('./smsAssessmentQuestion_datatable');
+
+        //contact.vars.assessment_batch
+        var assessmentQuestionCursor = getSMSQuestionCursor(contact.vars.assessment_batch);
+        if (assessmentQuestion == false){
+        	console.log("error assessment could not be started");
+        	sendReply("Assessment could not be started");
+        }
+
+        while(assessmentQuestionCursor.hasNext()){
+
+			var assessmentQuestion = assessmentQuestionCursor.next();
+			sendReply(assessmentQuestion.question_number "# "+ assessmentQuestion.question_text +
+        			"1. " + assessmentQuestion.choice_1 +
+        			"2. " + assessmentQuestion.choice_2 +
+        			"3. " + assessmentQuestion.choice_3
+        		  );
+
+	        waitForResponse('question'+assessmentQuestion.question_number, {
+	            timeoutMinutes: 1,
+	            timeoutId: 'timeout'
+	        });
+
+	        addResponseHandler('question'assessmentQuestion.question_number, function() {
+			    console.log(content + " is response");
+			    sendReply("answer response is "+content);
+			});
+
+			addTimeoutHandler('timeout', function() {
+			    sendReply("Your registration has been cancelled. To start the registration again, send JOIN.");
+			});
+
+
+        }
+        
+        
+        
+
+
+
         return true;
         
 	}else{
 	    return true;
 	}
 	
+}
+
+//return object or false
+function getSMSQuestionObject(batch_number){
+
+	return questionBase.getQuestion(batch_number);
+
 }

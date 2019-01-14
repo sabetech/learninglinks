8677f69299bcc73cf937cef558e634dd98d25c72
Albert <albert.mensahansah@gmail.com>
2019-01-14 10:10:04 +0000

    debugging

--- smsAssessmentQuestion_datatable.js
+++ smsAssessmentQuestion_datatable.js
@@ -3,31 +3,7 @@
  * to the `exports` variable.
  */
 
- exports = {
- 	getQuestionBatch: function(batch_number) { 
-		const DATATABLE_ID = "DT52d802fd4e601de0";
-
-		var questionTable = project.getDataTableById(DATATABLE_ID);
-
-		var cursor = questionTable.queryRows({
-		    vars: {
-		      'batch_number': batch_number
-		          }
-		});
-
-	 	cursor.limit(10);
-	  	//var questionObject = null;
-
-	  	console.log(cursor.hasNext())
-
-		if(!cursor.hasNext()){
-	    	return false;
-	    }
-	    return cursor;
-
-	},
-
-	getQuestionCursor: function(questionNumber, batch_number){
+ exports.getQuestionCursor = function(questionNumber, batch_number){
 		const DATATABLE_ID = "DT52d802fd4e601de0";
 
 		var questionTable = project.getDataTableById(DATATABLE_ID);
@@ -44,7 +20,4 @@
 		}
 		return cursor;
 
-
-	}
-
- }
+	};

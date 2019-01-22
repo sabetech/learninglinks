2fcbbd60075787e6513bfb6dd90b7fa32a0255d3
Albert <albert.mensahansah@gmail.com>
2019-01-22 20:59:52 +0000

    REMOVED LOGS

--- smsAssessment.js
+++ smsAssessment.js
@@ -41,7 +41,6 @@ global.main = function() {
         SMSquestionBase = require('./smsAssessmentQuestion_datatable');
 
         assessmentQuestionCursor = SMSquestionBase.getQuestionCursor(1, contact.vars.assessment_batch);
-     	console.log(assessmentQuestionCursor.next());
 
         if (assessmentQuestionCursor == false){
         	console.log("error assessment could not be started");
